08c52a7a4a81b283e2887d4dba36047351338529
Zahwa05 <zahwaaureliasalsabila254@gmail.com>
2026-05-08 19:32:13 +0700

    update project

---
+++ resources/views/mahasiswa/index.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Mahasiswa</title>
+</head>
+<body>
+    <table border="1">
+        <tr>
+            <th>Nama</th>
+            <th>NIM</th>
+            <th>Jurusan</th>
+        </tr>
+        @foreach ($data as $mhs)
+        <tr>
+            <td>{{ $m->nama }}</td>
+            <td>{{ $m->nim }}</td>
+            <td>{{ $m->jurusan }}</td>
+            <td>
+                <a href="/mahasiswa/edit/{{ $m->id }}">Edit</a>
+                <a href="/mahasiswa/delete/{{ $m->id }}">Hapus</a>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+
+    <h1>Edit Mahasiswa</h1>
+    <form action="/mahasiswa/update/{{ $m->id }}" method="POST">
+        @csrf
+        Nama: <input type="text" name="nama" value="{{ $m->nama }}"><br>
+        NIM: <input type="text" name="nim" value="{{ $m->nim }}"><br>
+        Jurusan: <input type="text" name="jurusan" value="{{ $m->jurusan }}"><br><button type="submit">Update</button>
+    </form>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ProdukController;
 
+use App\Http\Controllers\MahasiswaController;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -17,3 +19,11 @@ Route::get('/produk', [ProdukController::class, 'index']);
 Route::get('/showproduk', [ProdukController::class, 'show']);
 
 Route::get('/viewproduk', [ProdukController::class, 'view']);
+
+Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
+
+Route::get('/mahasiswa/create', [MahasiswaController::class, 'create']);
+
+Route::post('/mahasiswa/store', [MahasiswaController::class, 'store']);
+
+Route::get('/mahasiswa/delete/{id}', [MahasiswaController::class, 'destroy']);
